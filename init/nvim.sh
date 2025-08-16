83fa5d2433358640ed695bedba3e8772350f953d
viennamccarthy <145431446+viennamccarthy@users.noreply.github.com>
2025-08-16 18:03:41 +0100

    init scripts

---
+++ init/nvim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare NAME="nvim"
+declare EXEC
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo -e "Script must not be run as root."
+  exit 1
+elif ! EXEC=$(command -v $NAME); then
+  echo -e "$NAME not found in path."
+  exit 1
+fi
+
+if ! [ -e "${HOME}/.config/nvim/lua/plugins" ]; then
+  echo -e "No Lazy plugins found."
+  exit 1
+fi
+
+[ -e "${HOME}/.local/share/nvim/lazy" ] && exit 0
+
+set -eux
+
+$EXEC --headless "+Lazy! sync" +qa
